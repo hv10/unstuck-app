c8ba7a87570b78d64ab80b433d7477d7a951bd6f
hv10 <noeldanz@gmail.com>
2020-10-26 19:33:56 +0100

    Updates

---
+++ precache-manifest.7f35709f85cc0f7d6970e396138eb981.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8341514b88ebb70baa1e46b9eef226f",
+    "url": "/unstuck-app/index.html"
+  },
+  {
+    "revision": "8f2e818169a591455842",
+    "url": "/unstuck-app/static/css/main.a11b56fa.chunk.css"
+  },
+  {
+    "revision": "38cde678e63243644b66",
+    "url": "/unstuck-app/static/js/2.af26955e.chunk.js"
+  },
+  {
+    "revision": "5db129e9723f6c358336ece51034adaa",
+    "url": "/unstuck-app/static/js/2.af26955e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f2e818169a591455842",
+    "url": "/unstuck-app/static/js/main.a9b2fd7c.chunk.js"
+  },
+  {
+    "revision": "b5398c621e2f98f2cc22",
+    "url": "/unstuck-app/static/js/runtime-main.8cc3b7da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/unstuck-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e14b7b14ad74136280adea7ce64632f.js"
+  "/unstuck-app/precache-manifest.7f35709f85cc0f7d6970e396138eb981.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unstuck-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
